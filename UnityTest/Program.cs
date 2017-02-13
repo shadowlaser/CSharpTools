563841f68135c915cfa894b2ca075a38a500f467
yq_abbott <yang3911@126.com>
2017-02-13 09:02:00 +0800

    Unity ioc框架的使用&web.config自定义节的测试

---
+++ UnityTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Configuration;
+
+namespace UnityTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //IUnityContainer container = new UnityContainer();
+            ////container.RegisterType<IKiss, Boy>();
+            ////IKiss boy = container.Resolve<IKiss>();
+            ////Lily lily = new Lily(boy);
+            ////lily.kiss();
+            //UnityConfigurationSection configuration = ConfigurationManager.GetSection(UnityConfigurationSection.SectionName) as UnityConfigurationSection;
+            //configuration.Configure(container, "defaultContainer");
+            //var instances = container.Resolve<IKiss>();
+            //Lily lily = new Lily(instances);
+            //lily.kiss();
+
+            //web.config自定义节测试
+            TestSection ts = ConfigurationManager.GetSection("testsection") as TestSection;
+            Console.WriteLine(ts.name);
+
+
+            string output = JsonConvert.SerializeObject(ts,Formatting.Indented);
+
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+
+    public interface IKiss
+    {
+        void kiss();
+    }
+
+    public class Lily : IKiss
+    {
+        public IKiss boy;
+
+        public Lily(IKiss obj)
+        {
+            boy = obj;
+        }
+
+        public void kiss()
+        {
+            boy.kiss();
+            Console.WriteLine("lily kissing");
+        }
+    }
+
+    public class Boy : IKiss
+    {
+        public void kiss()
+        {
+            Console.WriteLine("boy kissing");
+        }
+    }
+}
---
+++ UnityTest/TestSection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace UnityTest
+{
+    class TestSection
+    {
+        public string name { get; set; }
+    }
+   
+
+    class TestSectionHandler: System.Configuration.IConfigurationSectionHandler
+    {
+        public object Create(object parent, object configContext, XmlNode section)
+        {
+            string siteName = section.SelectSingleNode("qq").InnerText;
+            
+            return new TestSection() { name = siteName };
+        }
+    }
+}
